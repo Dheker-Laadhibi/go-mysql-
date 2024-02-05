5cc3917d59cfe0d48bcbf41369d77440a638bfcd
dheker <dhekerlaadhibii@gmail.com>
2024-02-05 04:00:56 +0100

    first connection with mysql

---
+++ conn.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"fmt"
+	"log"
+)
+
+type Employee struct {
+	ID   int
+	Name string
+	City string
+}
+
+var db *sql.DB
+var err error
+
+// connection
+func dbConn() {
+	dbDrive := "mysql"
+	dbUser := "root"
+	dbPass := ""
+	dbName := "goblog"
+	db, err = sql.Open(dbDrive, dbUser+":"+dbPass+"@/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Vérification de la connexion à la base de données
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Définit les options de la connexion à la base de données
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+
+	// Vérifie si la connexion à la base de données a réussi
+	if err == nil {
+		fmt.Println("Connexion à la base de données réussie.")
+	} else {
+		fmt.Println("Échec de la connexion à la base de données.")
+	}
+}
+
+func getAllEmployees() []Employee {
+	row, err := db.Query("select * FROM employee")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// create instance of employee
+	emp := Employee{}
+	employees := []Employee{}
+	for row.Next() {
+		//scan copy the colomuns in the current row
+		err := row.Scan(&emp.ID, &emp.Name, &emp.City)
+		if err != nil {
+			log.Fatal(err)
+		}
+		employees = append(employees, emp)
+	}
+	return employees
+}
+
+func insert(name string, city string) {
+	stmt, err := db.Prepare("INSERT INTO employee(Name,City) values(?,?)")
+
+	if err != nil {
+
+		log.Fatal()
+	}
+	r, err := stmt.Exec(name, city)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// number of rows affected 
+	affectedRows, err := r.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	fmt.Printf("statement affected  %d rows\n ", affectedRows)
+}
+func main() {
+	dbConn()
+	fmt.Println(getAllEmployees())
+	insert("zizou ","algeria ")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dhekerlaadhibi/LearnGo/mysql
+
+go 1.21.6
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect
